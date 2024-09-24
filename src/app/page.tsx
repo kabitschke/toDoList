851d404eebbe43ffcd6f7d7f850df0e43d315ba1
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-09-24 04:59:47 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,122 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  type Todo = {
+    label: string;
+    checked: boolean;
+  }
+
+
+  const [texto, setTexto] = useState('');
+  const [list, setList] = useState<Todo[]>([]);
+  const [indice, setIndice] = useState<number | null>(null);
+
+  const handleEnter = (e:React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter'){
+      handleAdd();
+      
+    }
+
+  }
+
+
+  const handleAdd = () => {
+
+    if (texto.trim() === '')return;
+
+    
+
+    if (indice !== null){   
+      const up = list.map((el, key) => (
+        key === indice ? {...el, label:texto, checked: false}: el)
+   );
+
+      
+
+      setList(up);
+      setTexto('');
+      setIndice(null);
+    }else{
+      setList([...list, {label: texto, checked: false}]);
+      setTexto('');
+      
+    }
+
+
+  }
+
+  const handleDell = (index:number) => {
+    
+    if(confirm('Deseja excluir?')=== true){
+      
+      const up = list.filter((el, key) => key !== index);
+      setList(up);
+    }
+
+  }
+
+  const handleAlter = (index:number) => {
+    setIndice(index);
+    setTexto(list[index].label);
+
+  }
+
+
+  const toggleItem = (index:number) => {
+
+    for (let i in list){
+      if(list[index] === list[i]){
+        list[index].checked = !list[index].checked
+      }
+    }
+
+    setList([...list]);
+
+  }
+
+
+  return (
+    <div className="w-screen h-screen bg-[#222] flex flex-col items-center">
+      <div className="max-w-3xl w-full bg-[#333] flex items-center justify-center mt-8 p-8 rounded-md">
+
+            <input type="text " 
+            className="p-2 rounded-md mr-4 flex-1"
+            placeholder="O que deseja fazer?"
+            value={texto}
+            onKeyDown={handleEnter}
+            onChange={e => setTexto(e.target.value)
+                
+            }
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+            <button className="bg-[#222] hover:bg-[#111] text-white px-4 py-2 rounded-md"
+            onClick={handleAdd}
+            >{`${indice !== null ? 'Editar' : 'Adicionar'}`}</button>
+      </div>   
+
+      <div className="max-w-3xl w-full mt-4">
+        {
+          list.map((el, key) => 
+          <div key={key} className="text-white flex justify-between items-center bg-[#333] mt-4 rounded-md">
+            <div className={`mt-4 ml-2 ${el.checked === true ? 'line-through': ''}`}>
+              <input type="checkbox"  checked={el.checked} className="w-4 h-4" onClick={()=> toggleItem(key)}/> {el.label}</div>
+
+            <div className="">
+              <span className="p-2 bg-yellow-600 rounded-md cursor-pointer mr-2"
+              onClick={() => handleAlter(key)}>Alterar</span>
+
+              <span className="p-2 bg-red-600 rounded-md cursor-pointer mr-2"
+              onClick={()=> handleDell(key)}>Deletar</span>
+
+            </div>
+
+          </div>
+          )
+        }  
+      </div>   
     </div>
   );
 }
